2a1f888f81fb95db83a9667502825f4400a5abfc
benasphy <binidani1903@gmail.com>
2025-05-20 10:06:11 +0300

    Fix syntax error in tf_compatibility.py

--- utils/tf_compatibility.py
+++ utils/tf_compatibility.py
@@ -8,14 +8,15 @@ class TensorFlowNotAvailable:
     
     def __getattr__(self, name):
         if not self._warned:
-            warnings.warn("""
+            warning_msg = """
             🚨 TensorFlow is not available in this environment.
             This project requires TensorFlow, which is not supported in Streamlit Cloud.
             Please try:
             1. Running this project locally
             2. Using a different deployment platform
             3. Selecting a project that doesn't require TensorFlow
-            "", RuntimeWarning)
+            """
+            warnings.warn(warning_msg, RuntimeWarning)
             self._warned = True
         raise ImportError("TensorFlow is not available in this environment")
 
